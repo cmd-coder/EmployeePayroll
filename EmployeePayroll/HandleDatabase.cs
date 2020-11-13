794167add2209d7af1d697b03bd8f12c2dfa2f72
CMD <gautamkailashsingh@gmail.com>
2020-11-13 22:02:31 +0530

    [Gautam] Added the sql connection string to connect to the database.

---
+++ EmployeePayroll/HandleDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    class HandleDatabase
+    {
+        public static SqlConnection ConnectionSetUp()
+        {
+            return new SqlConnection(@"Data Source=DESKTOP-DKOUJ1R\SQLEXPRESS;Initial Catalog=payroll_service;Integrated Security=True");
+        }
+    }
+}
