74e881e7e6114352feabe56a58825c778dd4db54
CMD <gautamkailashsingh@gmail.com>
2020-11-14 19:07:50 +0530

    [Gautam] Updated the databse with new basic pay.

--- EmployeePayroll/HandleDatabase.cs
+++ EmployeePayroll/HandleDatabase.cs
@@ -45,5 +45,42 @@ namespace EmployeePayroll
                 System.Console.WriteLine(e.Message);
             }
         }
+
+        public static void UpdateDataBase(string name, int newBasicPay)
+        {
+            SqlConnection sqlConnection = ConnectionSetUp();
+            try
+            {
+                using (sqlConnection)
+                {
+                    string query = "update payroll_details set payroll_details.basic_pay = "+newBasicPay+" from payroll_details p inner join employee_details e on e.salid=p.salid where e.name='"+name+"';";
+                    SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                    sqlConnection.Open();
+                    cmd.ExecuteNonQuery();
+                    string queryCheck = "select e.name, e.start_date, e.gender, e.phone, e.address ," +
+                        " c.department_name, p.basic_pay, p.deduction, p.taxable, p.income_tax, " +
+                        "p.net_pay from employee_details e inner join payroll_details p on p.salid=e.salid" +
+                        " inner join department d on d.empid=e.empid inner join company c on d.department_id=c.department_id;";
+                    cmd = new SqlCommand(queryCheck, sqlConnection);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        System.Console.WriteLine("Name -- Start Date -- Gender -- Phone -- Address -- Department Name -- Basic Pay -- Deduction -- Taxable Pay -- Income Tax -- Net Pay");
+                        while (dr.Read())
+                        {
+                            Console.WriteLine(dr.GetString(0) + " -- " + dr.GetDateTime(1) + " -- " + dr.GetString(2) + " -- " + dr.GetString(3) + " -- " + dr.GetString(4) + " -- " + dr.GetString(5) + " -- " + dr.GetInt32(6) + " -- " + dr.GetInt32(7) + " -- " + dr.GetInt32(8) + " -- " + dr.GetInt32(9) + " -- " + dr.GetInt32(10));
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("No data found");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
     }
 }
--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -8,6 +8,10 @@ namespace EmployeePayroll
         {
             Console.WriteLine("Welcome to Employee Payroll Service.");
             HandleDatabase.RetrieveFromDataBase();
+            Console.WriteLine("Enter the name and the updated basic pay of the employee");
+            string name = Console.ReadLine();
+            int newBasicPay = Convert.ToInt32(Console.ReadLine());
+            HandleDatabase.UpdateDataBase(name, newBasicPay);
         }
     }
 }
